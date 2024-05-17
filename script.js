56fce9020ebe8765c6c556cc326c659d859a760d
Kulvinder5665 <160815444+Kulvinder5665@users.noreply.github.com>
2024-05-17 23:42:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+ function showSlidebar(){
+          const sidebar= document.querySelector('.sidebar')
+          sidebar.style.display ='flex';
+      }
+      function hideSidebar(){
+          const sidebar= document.querySelector('.sidebar')
+          sidebar.style.display ='none';
+      }
+
+      function showSlidebar(){
+        const sidebar= document.querySelector('.sidebar')
+        sidebar.style.display ='flex';
+    }
+    function hideSidebar(){
+        const sidebar= document.querySelector('.sidebar')
+        sidebar.style.display ='none';
+    }
+
+
